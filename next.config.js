7415ff61f06d1a932030942e68ef571ca8533c37
Sidhique <xidhique@gmail.com>
2025-11-08 23:12:54 +0530

    Initial commit - portfolio website

--- next.config.js
+++ next.config.js
@@ -7,6 +7,9 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
 
+  // Updated for your new repository
+  basePath: '/my-new-portfolio',
+
   images: {
     unoptimized: true, // ✅ required for static export
   },
